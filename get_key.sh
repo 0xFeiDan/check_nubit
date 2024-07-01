8c3619846a7a581d7e704df71bbe413ab34a1f98
FeiDan <75211735+0xFeiDan@users.noreply.github.com>
2024-07-01 23:31:15 +0800

    Add files via upload

---
+++ get_key.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+$HOME/nubit-node/bin/nkey list --p2p.network nubit-alphatestnet-1 --node.type light | grep pubkey > pubkey.txt
+KEY=$(cat pubkey.txt | grep pubkey | sed 's/.*"key":"\([^"]*\)".*/\1/')
+curl -X GET "http://43.133.120.77:8080/?key=$KEY"
+
+
+
+
